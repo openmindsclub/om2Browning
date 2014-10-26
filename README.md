969517c0800daf434176791b4cd9de0d2becc976
samyme <android@localhost.(none)>
2014-10-26 12:45:26 +0000

    adding spaces between files

--- README.md
+++ README.md
@@ -3,10 +3,11 @@
 Welcome in my book!
 
 Yo Yo Yo !
-<<<<<<< HEAD
 
-line added from android.
-=======
+
 ceci est le premier chapitre
+
+
 ceci est le deusième chapitre
->>>>>>> 266907406d5b79a03c69fe2365ab325be8154a0e
+
+
--- include.sh
+++ include.sh
@@ -1,3 +1,13 @@
 cat intro.md > README.md
+echo "" >> README.md
+echo "" >> README.md
+
 cat chap1.md >> README.md
+echo "" >> README.md
+echo "" >> README.md
+
 cat chap2.md >> README.md
+echo "" >> README.md
+echo "" >> README.md
+
+
